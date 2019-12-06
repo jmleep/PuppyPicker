1f7633e1eb149c7a3df17fe83fdf630bc413ea01
Jordan Leeper <j.m.leep@gmail.com>
2019-12-06 17:42:53 -0500

    Initial Commit

---
+++ FavoriteDogBreeds/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FavoriteDogBreeds
+//
+//  Created by Jordan Leeper on 12/6/19.
+//  Copyright © 2019 Jordan Leeper. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FavoriteDogBreeds/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FavoriteDogBreeds
+//
+//  Created by Jordan Leeper on 12/6/19.
+//  Copyright © 2019 Jordan Leeper. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
